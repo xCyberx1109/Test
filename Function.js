9ec903ebf75ddf4b7f2ac3b63ce450b5d983373e
xCyberx1109 <93750260+xCyberx1109@users.noreply.github.com>
2024-10-14 16:18:59 +0700

    Delete Function.js

--- Function.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("sound-button").addEventListener("click", function () {
-    document.getElementById("sound").play();
-    var audio = document.getElementById("myAudio");
-    audio.volume = 0.05; 
-});
